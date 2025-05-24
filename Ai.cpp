875db58849950ed6efa56770d853d0838877ff1d
sunnycastle09 <sunnycastle0901@gmail.com>
2025-05-24 19:58:12 +0900

    2차원에서 1차원으로

--- Ai.cpp
+++ Ai.cpp
@@ -8,22 +8,17 @@ void Data();
 std::vector<std::string> data;
 std::string Adata = "i ate apple/i ate cat/i love cat/";
 std::vector<std::string> Avector;
-std::vector<std::vector<std::string>> Bvector;
+std::vector<std::string> Bvector;
 std::string word;       
 
 int main()
-{    ////////////////////////////////////////       중요한거: 그냥 seekg()쓰는법 배워서 ssi커서 위치 바꾸는 법 배워라
-    //Avector.push_back("i");// 초기값 설정
-    //Bvector[0].push_back("ate");
-    //Avector.push_back("ate");
-    //Bvector[1].push_back("apple");
+{   
     sentense();
     int b = 0, num = 0;
     std::string tmp;
-    Bvector.resize(100);
     for (int i = 1;i < data.size();i++) {
         std::istringstream iss(data[i]);
-        std::istringstream a(data[i]);
+        std::istringstream a(data[i]);  
         while (a>>tmp) {
             num++;
         }
@@ -31,13 +26,13 @@ int main()
         while (1) {
             Avector.push_back(word);
             iss >> word;
-            Bvector[b].push_back(word);
+            Bvector.push_back(word);
             b++;
             if (num==b+1)
                 break;
         }
     }
-    std::cout << Avector[0]<<" " << Bvector[0][0] << " " << Avector[1] << " " << Bvector[1][0];
+    std::cout << Avector[0] << " " << Bvector[0] << " " << Avector[1] << " " << Bvector[1];
 }
 
 void Data() {
