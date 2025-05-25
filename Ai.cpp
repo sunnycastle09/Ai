e5641956b31a5f1f0750322a99b76c972fa02832
sunnycastle09 <sunnycastle0901@gmail.com>
2025-05-26 00:44:19 +0900

    종료조건만

--- Ai.cpp
+++ Ai.cpp
@@ -1,25 +1,69 @@
 ﻿#include <iostream>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 void sentense();
 void Data();
 
+std::string Adata = "i ate z/i love z/i ate z/ilove a/i love a/i ate z/";
 std::vector<std::string> data;
-std::string Adata = "i ate apple/i ate cat/i love cat/";
 std::vector<std::string> Avector;
 std::vector<std::string> Bvector;
-std::string word;       
+std::vector<std::string> Sword;
+std::vector<std::string> s_count;
+std::vector<int> count;
+std::string word;
+std::string output;
+std::string Aword;
 
 int main()
 {   
     sentense();
+    Data();
+    std::string source;
+    std::cin >> source;
+    Aword = source;
+    int j = 0;
+    while (1) { ///////////////////       조건만 하면 완성??
+        Sword.clear();
+        s_count.clear();
+        count.clear();
+        if (j > 1)
+            source = output;
+        if (j == 0) {
+            std::cout <<"\n시작\n"<< Aword << " "; //source다음 어절이 output 전 output과 후 output이 같으면 됨
+            j++;
+            continue;
+        }
+        for (int i = 0;i < Avector.size();i++) { //Sword에 source다음 어절 넣음
+            if (source == Avector[i]) {
+                Sword.push_back(Bvector[i]);
+            }
+        }
+        for (int i = 0;i < Sword.size();i++) { //Sword(source다음어절)에 각 어절이 몇개 있는지 저장
+            s_count.push_back(Sword[i]);
+            count.push_back(std::count(Sword.begin(), Sword.end(), Sword[i]));
+        }
+        auto max = std::max_element(count.begin(), count.end());
+        for (int i = 0;i < count.size();i++) { //max비교로 가장 많이 나온 어절 찾아 output에 저장 
+            if (count[i] == *max) {
+                output = s_count[i];
+            }
+        }
+            std::cout << output << " ";
+            j++;
+    }
+}
+void Data() {
     int b = 0, num = 0;
     std::string tmp;
-    for (int i = 1;i < data.size();i++) {
+    for (int i = 0;i < data.size();i++) {
+        b = 0;
+        num = 0;
         std::istringstream iss(data[i]);
-        std::istringstream a(data[i]);  
-        while (a>>tmp) {
+        std::istringstream a(data[i]);
+        while (a >> tmp) {
             num++;
         }
         iss >> word;
@@ -28,16 +72,11 @@ int main()
             iss >> word;
             Bvector.push_back(word);
             b++;
-            if (num==b+1)
+            if (num == b + 1)
                 break;
         }
     }
-    std::cout << Avector[0] << " " << Bvector[0] << " " << Avector[1] << " " << Bvector[1];
 }
-
-void Data() {
-}
-
 void sentense() { // Populate the vector with sentences
    int i = 0;
    std::string tmp;
